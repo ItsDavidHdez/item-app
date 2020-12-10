f5235173ed3040570b1da3d0351ff3c8e042b171
David Vargas Hernandez <david-0610@live.com.mx>
2020-12-10 11:47:30 -0600

    Stack navigator created

---
+++ screens/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ScrollView, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+const Login = () => {
+  return (
+    <ScrollView>
+      <View>
+        <TextInput />
+      </View>
+    </ScrollView>
+  );
+};
