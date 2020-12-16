57c21ee3f771ae2b6771b615e7ebd6c5b314eb27
David Vargas Hernandez <david-0610@live.com.mx>
2020-12-16 02:23:52 -0600

    Gradient done

--- screens/Login.js
+++ screens/Login.js
@@ -1,37 +1,48 @@
 import React from "react";
 import { ScrollView, View, Button, StyleSheet, Text } from "react-native";
-import { CheckBox } from "react-native-elements";
 import { TextInput } from "react-native-gesture-handler";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Login = () => {
   return (
-    <ScrollView style={styles.container}>
-      <ScrollView style={styles.inputGroup}>
-        <Text style={styles.labels}>Username</Text>
-        <View style={styles.form}>
-          <TextInput color="white" />
-        </View>
-        <View style={styles.form}>
-          <Text style={styles.labels}>Password</Text>
-          <TextInput color="white" />
+    <LinearGradient
+      colors={["#212D4C", "#0D0E1B", "#192f6a"]}
+      style={styles.linearGradient}
+    >
+      <ScrollView style={styles.container}>
+        <ScrollView style={styles.inputGroup}>
+          <Text style={styles.labels}>Username</Text>
+          <View style={styles.form}>
+            <TextInput color="white" />
+          </View>
+          <View style={styles.form}>
+            <Text style={styles.labels}>Password</Text>
+            <TextInput color="white" />
+          </View>
+        </ScrollView>
+        <View style={styles.buttons}>
+          <Button
+            title="Login"
+            color="#212941"
+            onPress={() => console.log(true)}
+          />
         </View>
       </ScrollView>
-      <View style={styles.buttons}>
-        <Button
-          title="Login"
-          color="#212941"
-          onPress={() => console.log(true)}
-        />
-      </View>
-    </ScrollView>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    // paddingLeft: 15,
+    // paddingRight: 15,
+    // borderRadius: 5,
+  },
   container: {
     flex: 1,
     padding: 35,
-    backgroundColor: "#0D0E1B",
+    // backgroundColor: "#0D0E1B",
     color: "#0000",
   },
   labels: {
@@ -43,7 +54,7 @@ const styles = StyleSheet.create({
     padding: 0,
     marginBottom: 15,
     borderBottomColor: "#ccc",
-    backgroundColor: "#212941",
+    // backgroundColor: "#212941",
     padding: 10,
   },
   form: {
@@ -57,7 +68,7 @@ const styles = StyleSheet.create({
     flex: 1,
     // flexDirection: "row",
     justifyContent: "center",
-    backgroundColor: "#12172A",
+    // backgroundColor: "#12172A",
   },
 });
 
